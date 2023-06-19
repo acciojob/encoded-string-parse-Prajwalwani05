4aa7b4efbe114b74072b45873b4b7c17eb4f9f00
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-06-19 20:05:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const parseCode = (str) => {
 	const [firstName, lastName, id] = str.split('000');
 
   return {
-    firstName,
-    lastName,
-    id
+  firstName: firstName,
+    lastName: lastName,
+    id: id
   };
 };
 
 // Do not change the code below
 const str = prompt("Enter str: ");
-alert(JSON.stringify(parseCode(str)));
+alert(JSON.stringfy.(parseCode(str)));
